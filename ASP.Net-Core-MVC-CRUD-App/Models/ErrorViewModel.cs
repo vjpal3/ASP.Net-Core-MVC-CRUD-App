496952bb284a85a76b9b74ced0c714059ee589fc
vjpal3 <vrishalipal@yahoo.com>
2019-11-20 16:53:40 -0500

    Add project files.

---
+++ ASP.Net-Core-MVC-CRUD-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.Net_Core_MVC_CRUD_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
