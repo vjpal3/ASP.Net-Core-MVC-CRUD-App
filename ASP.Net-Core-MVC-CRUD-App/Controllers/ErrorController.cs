51090b178f3a48cffd8e86195cf4c3f39eefd835
vjpal3 <vrishalipal@yahoo.com>
2019-11-29 10:32:04 -0500

    Centralised 404 error handling

---
+++ ASP.Net-Core-MVC-CRUD-App/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_Core_MVC_CRUD_App.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // If there is 404 status code, the route path will become Error/404
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found";
+                    break;
+            }
+
+            return View("NotFound");
+        }
+    }
+}
--- ASP.Net-Core-MVC-CRUD-App/Startup.cs
+++ ASP.Net-Core-MVC-CRUD-App/Startup.cs
@@ -50,9 +50,7 @@ namespace ASP.Net_Core_MVC_CRUD_App
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
             }
 
             app.UseHttpsRedirection();
