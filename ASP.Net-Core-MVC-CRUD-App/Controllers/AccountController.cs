5426067a2838e9a9cfc271179faf1d21b1006760
vjpal3 <vrishalipal@yahoo.com>
2019-12-14 23:07:33 -0500

    Add Account Controller and corrosponding Register view

---
+++ ASP.Net-Core-MVC-CRUD-App/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_Core_MVC_CRUD_App.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
