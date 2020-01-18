bff1f2f8730be76ff31ae15f55ba0ecce4b80952
vjpal3 <vrishalipal@yahoo.com>
2020-01-17 20:42:08 -0500

    Add custom validation attribute

--- ASP.Net-Core-MVC-CRUD-App/ViewModels/RegisterViewModel.cs
+++ ASP.Net-Core-MVC-CRUD-App/ViewModels/RegisterViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.Net_Core_MVC_CRUD_App.Utilities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.Net_Core_MVC_CRUD_App.ViewModels
@@ -12,6 +13,7 @@ namespace ASP.Net_Core_MVC_CRUD_App.ViewModels
         [Required]
         [EmailAddress]
         [Remote(action: "IsEmailInUse", controller: "Account")]
+        [ValidEmailDomain(allowedDomain: "softec.com", ErrorMessage = "Email domain must be softec.com")]
         public string Email { get; set; }
 
         [Required]
