58f028a0d191acbbb9f40b202b9ea8883abf14b3
vjpal3 <vrishalipal@yahoo.com>
2020-02-16 09:09:04 -0500

    Create Identity roles

---
+++ ASP.Net-Core-MVC-CRUD-App/Controllers/AdministrationController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.Net_Core_MVC_CRUD_App.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_Core_MVC_CRUD_App.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager) 
+        {
+            this.roleManager = roleManager;
+        }
+        
+        [HttpGet]
+        public IActionResult CreateRole ()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateRole (CreateRoleViewModel model)
+        {
+           if(ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+                
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            // The view is rendered with validation errors for user to fix.
+            return View(model);
+        }
+    }
+}
